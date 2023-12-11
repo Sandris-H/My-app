3a3e4847bb03814d0c61ed6027c1bd7f873a94c3
Aliaksandr Hanchar <ahanchar808@gmail.com>
2023-12-11 18:57:37 +0100

    added main logic

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export function AboutPage() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat similique
+      nisi aspernatur et quasi repellendus quod delectus, non reiciendis sequi
+      ab quae totam molestiae natus itaque at eveniet eos odit sed earum,
+      nesciunt reprehenderit. Provident deserunt molestias illo libero vitae
+      fuga delectus, laudantium ut voluptatem, rerum laboriosam dicta quisquam
+      perferendis?
+    </div>
+  );
+}
